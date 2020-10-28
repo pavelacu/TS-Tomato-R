571fc7b97368739478ca3619aa9307e58bdc954e
Pavel Acu <pavelacu@gmail.com>
2020-10-28 07:57:12 -0600

    Add Analysis Time Series

--- Time Series Analysis Tomato Price.R
+++ Time Series Analysis Tomato Price.R
@@ -1,29 +1,40 @@
 #Libraries
 library('ggplot2')
+library('ggfortify')
 library('forecast')
 library('tseries')
+#https://rpubs.com/palominoM/series
+
+
+# load data
 datos = read.csv('tomato_price.csv', header=TRUE)
 head(datos)
 
+# data Transform to TimeSeries (January, 1 2016 to June, 1 2020)
+myts <- ts(datos$rate, start=c(2012, 1), end=c(2020, 6), frequency=12, class="ts", name = "Tomato Rate")
 
+# Chart Time Series
+autoplot(myts, ts.colour = 'green', ts.linetype = 'dashed')
 
-plot(datos$month, datos$rate, main="Semana",ylab="Ventas")
+# Test de Normalidad: Histogram, Kolmorov-Smirnov, Shapiro
+hist(datos$rate, col='grey')
+ks.test(datos$rate, "pnorm", mean=mean(datos$rate), sd=sd(datos$rate))
+shapiro.test(datos$rate)
 
-myts <- ts(datos$rate, start=c(2012, 1), end=c(2020, 6), frequency=12)
-head(myts)
-plot(myts)
-desc(myts)
-ggplot(data = datos, aes(x = month, y = rate))+geom_line(color = "#00AFBB", size = 2)
+# Serie Estacionaria
+autoplot(acf(myts, plot = FALSE))
+autoplot(stl(myts, s.window = "periodic"), ts.colour = "blue")
+# Test Dickey-Fuller
+adf.test(datos$rate, alternative = "stationary")
 
+# Descomponiendo serie por año
+price_ma = ts(na.omit(datos$rate), frequency=12)
+decomp = stl(price_ma, s.window="periodic")
+deseasonal_ma <- seasadj(decomp)
+plot(decomp)
 
-
-n_lags = 3
+n_lags = 0
 # Autocorrelacion (No tomar en cuenta para valor para x=0)
-a <-(acf(datos$ventas, lag = n_lags, plot = TRUE))
+a <-(acf(datos$rate,plot = TRUE, main =""))
 # convert to data frame
-a <- data.frame(lag=a$lag, acf=a$acf)
-# use data frame for ggplot
-# 95% confidence interval limits
-ci <- qnorm((1 + 0.95)/2)/sqrt(sum(!is.na(datos)))  
-ggplot(a, aes(lag, acf)) + geom_area(fill="grey") +  geom_hline(yintercept=c(ci, -ci), linetype="dashed") +
-  theme_bw()
+a <-(pacf(datos$rate, plot = TRUE, main =""))
